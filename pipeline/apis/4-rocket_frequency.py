e259e311cbfb80b21a1754235992ad7e7acc7108
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-10-26 16:28:23 +0200

    rocket_frequency

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
 """Script that displays the number of launches per rocket"""
 
-
 import requests
 from collections import defaultdict
 
-
 def get_launches_per_rocket():
-    """Script that displays the number of launches per rocket"""
-
     launches_url = 'https://api.spacexdata.com/v4/launches'
     rockets_url = 'https://api.spacexdata.com/v4/rockets'
 
     try:
-        launches_response = requests.get(launches_url)
-        launches_response.raise_for_status()
-        launches = launches_response.json()
-
+        launches = requests.get(launches_url).json()
         launch_count = defaultdict(int)
+
         for launch in launches:
             rocket_id = launch['rocket']
             launch_count[rocket_id] += 1
 
-        rockets_response = requests.get(rockets_url)
-        rockets_response.raise_for_status()
-        rockets = rockets_response.json()
-
+        rockets = requests.get(rockets_url).json()
         rocket_names = {rocket['id']: rocket['name'] for rocket in rockets}
 
         rocket_launches = [
@@ -36,14 +27,12 @@ def get_launches_per_rocket():
         rocket_launches.sort(key=lambda x: (-x[1], x[0]))
 
         for rocket, count in rocket_launches:
-            print("{}: {}".format(rocket, count))
+            print(f"{rocket}: {count}")
 
     except requests.RequestException as e:
-        print(
-            'An error occurred while making an API request: {}'.format(e))
+        print(f'An error occurred while making an API request: {e}')
     except Exception as err:
-        print('A general error occurred: {}'.format(err))
-
+        print(f'A general error occurred: {err}')
 
 if __name__ == '__main__':
     get_launches_per_rocket()
