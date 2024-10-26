8a669123da783bd3ded457846858736858994cb3
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-10-26 16:44:42 +0200

    Update 3-upcoming.py

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -18,13 +18,14 @@ def get_upcoming_launch():
         upcoming_launch = sorted(launches, key=lambda x: x['date_unix'])[0]
 
         rocket_id = upcoming_launch['rocket']
-        rocket_url = f'https://api.spacexdata.com/v4/rockets/{rocket_id}'
+        rocket_url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket_id)
         rocket_response = requests.get(rocket_url)
         rocket_response.raise_for_status()
-        rocket_name = rocket_response.json()['name']
+        rocket = rocket_response.json()
+        rocket_name = rocket['name']
 
         launchpad_id = upcoming_launch['launchpad']
-        launchpad_url = f'https://api.spacexdata.com/v4/launchpads/{launchpad_id}'
+        launchpad_url = 'https://api.spacexdata.com/v4/launchpads/{}'.format(launchpad_id)
         launchpad_response = requests.get(launchpad_url)
         launchpad_response.raise_for_status()
         launchpad = launchpad_response.json()
@@ -42,9 +43,9 @@ def get_upcoming_launch():
                 launchpad_name, launchpad_locality))
 
     except requests.RequestException as e:
-        print(f'An error occurred while making an API request: {e}')
+        print('An error occurred while making an API request: {}'.format(e))
     except Exception as err:
-        print(f'A general error occurred: {err}')
+        print('A general error occurred: {}'.format(err))
 
 if __name__ == '__main__':
     get_upcoming_launch()
