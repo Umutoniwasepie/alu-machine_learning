d9d3bba5594651a2f71b17289ac7454970d7a977
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-10-26 16:52:24 +0200

    user_location

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-"""Script that prints the location of a specific user"""
+
+""" Return location of a GitHub user or provide rate limit informatin"""
 
 import requests
 import sys
 import time
+
 
 if __name__ == "__main__":
     res = requests.get(sys.argv[1])
@@ -12,11 +14,12 @@ if __name__ == "__main__":
     if res.status_code == 403:
         rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
         current_time = int(time.time())
-        diff = int((rate_limit - current_time) / 60)
-        print('Reset in {} min'.format(diff))
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
     elif res.status_code == 404:
         print("Not found")
     elif res.status_code == 200:
         res = res.json()
-        location = res.get('location', 'Location not available')
-        print(location)
+        print(res['location'])
