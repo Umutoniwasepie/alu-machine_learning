d4b63c0653a6d5eb4c0042de3ae7857e4cf1a1d8
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2025-01-10 17:31:25 +0200

    Made some changes

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -236,7 +236,7 @@ class NST:
         one, h, w, c = style_output.shape
         if not isinstance(gram_target, (tf.Tensor, tf.Variable)) or \
            len(gram_target.shape) is not 3 or gram_target.shape != (1, c, c):
-[O            raise TypeError(
+            raise TypeError(
                 "gram_target must be a tensor of shape [1, {}, {}]".format(
                     c, c))
         gram_style = self.gram_matrix(style_output)
